f2810c21f3771650a333f25dec0a1dfc1dd74c51
LookAtYourSkill <66884858+LookAtYourSKill@users.noreply.github.com>
2021-11-06 21:09:33 +0100

    Update file to speech.py

--- file to speech.py
+++ file to speech.py
@@ -3,10 +3,10 @@ import os
 
 path = 'path where the mp3 file should be saved'
 
-file = input('Welches File möchtest du vorgelesen bekommen? (Full Path where the file is)')
+file = input('Which file would you like to have read to you? ? (Full Path where the file is)')
 file_open = open(str(file), "r").read().replace("\n", " ")
 
-language = input('Welche Sprache möchtest du? \nde/en\n')
+language = input('What language you want? \nde = german/en = english\n')
 if str(language) == 'en':
     language = 'en'
 else:
@@ -14,6 +14,6 @@ else:
 
 speech = gTTS(text=str(file_open), lang=language, slow=False)
 
-name = input('Wie soll das File heißen?')
+name = input('What name should the file have?')
 speech.save(path+'F2S'+str(name)+'.mp3')
 os.system(f'start {path}F2S{str(name)}.mp3')
